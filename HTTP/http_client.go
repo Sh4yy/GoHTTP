e4d4d59f71f5cdb159718d2a3241a9bf9ccdcdcd
shayan <shayan@umd.edu>
2020-11-01 20:54:51 -0500

    updated to http 1.1

--- HTTP/http_client.go
+++ HTTP/http_client.go
@@ -1,32 +1,14 @@
 package HTTP
 
 import (
-	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net"
-	netUrl "net/url"
-)
-
-var (
-	clrf    = []byte("\r\n")
-	portMap = map[string]string{
-		"http":  "80",
-		"https": "443",
-	}
 )
 
 type HTTPClient struct {
 	Protocol  string
 	UserAgent string
-}
-
-type Request struct {
-	Address string
-	Path    string
-	Method  string
-	Headers map[string]string
-	Body    []byte
 }
 
 func NewClient() *HTTPClient {
@@ -36,43 +18,16 @@ func NewClient() *HTTPClient {
 	}
 }
 
-func (http HTTPClient) constructRequest(request *Request) (*bytes.Buffer, error) {
-
-	var buf bytes.Buffer
-
-	// write headers
-	_, err := fmt.Fprintf(&buf, "%s %s %s", request.Method, request.Path, http.Protocol)
-	if err != nil {
-		return nil, err
-	}
-	buf.Write(clrf)
-
-	for key, value := range request.Headers {
-		fmt.Fprintf(&buf, "%s: %s", key, value)
-		buf.Write(clrf)
-	}
-
-	// terminate headers
-	buf.Write(clrf)
-
-	// write Body
-	if len(request.Body) != 0 {
-		buf.Write(request.Body)
-	}
-
-	return &buf, nil
-
-}
 
 func (http HTTPClient) Send(request *Request) ([]byte, error) {
 
-	buf, err := http.constructRequest(request)
+	buf, err := request.Serialize()
 	if err != nil {
 		return nil, err
 	}
 
 	fmt.Println(buf)
-	conn, err := net.Dial("tcp", request.Address)
+	conn, err := net.Dial("tcp", request.GetAddress())
 	if err != nil {
 		return nil, err
 	}
@@ -91,44 +46,11 @@ func (http HTTPClient) Send(request *Request) ([]byte, error) {
 
 }
 
-func (http HTTPClient) resolveIP(url string) (string, string, string) {
-
-	parsedURL, err := netUrl.Parse(url)
-	if err != nil {
-		panic(err)
-	}
-
-	port := parsedURL.Port()
-	if port == "" {
-		port = portMap[parsedURL.Scheme]
-	}
-
-	path := parsedURL.RawPath
-	if path == "" {
-		path = "/"
-	}
-
-	return parsedURL.Hostname(), port, path
-
-}
-
 func (http HTTPClient) GET(url string) ([]byte, error) {
-
-	address, port, path := http.resolveIP(url)
-	fmt.Println(net.JoinHostPort(address, port))
-
-	request := Request{
-		Address: net.JoinHostPort(address, port),
-		Path:    path,
-		Method:  "GET",
-		Headers: map[string]string{
-			"User-Agent": http.UserAgent,
-			"Accept":     "*/*",
-			"Host":       address,
-		},
-		Body: []byte{},
-	}
-
-	return http.Send(&request)
-
+	request := NewRequest(url, "GET")
+	request.WriteHeader("User-Agent", http.UserAgent)
+	request.WriteHeader("Accept", "*/*")
+	request.WriteHeader("Host", request.GetHostname())
+	request.WriteHeader("Connection", "close")
+	return http.Send(request)
 }
---
+++ HTTP/request.go
@@ -0,0 +1,110 @@
+package HTTP
+
+import (
+	"bytes"
+	"fmt"
+	"net"
+	NetURL "net/url"
+	"strconv"
+)
+
+var (
+	PortMap = map[string]string{
+		"http":  "80",
+		"https": "443",
+	}
+	CLRF = []byte("\r\n")
+)
+
+type Request struct {
+	Protocol string
+	URL		 string
+	Method   string
+	Headers  map[string]string
+	Body     []byte
+}
+
+func NewRequest(url, method string) *Request {
+	return &Request{
+		Protocol: "HTTP/1.1",
+		URL: url,
+		Method: method,
+		Headers: map[string]string{},
+	}
+}
+
+func (request *Request) WriteBody(body []byte) {
+	request.Body = body
+	request.WriteHeader("Content-Length", strconv.Itoa(len(body)))
+}
+
+func (request *Request) WriteHeader(key, value string) {
+	request.Headers[key] = value
+}
+
+// urlParser parses url and returns (hostname, port, path, scheme)
+func urlParser(url string) (string, string, string, string) {
+	parsedURL, err := NetURL.Parse(url)
+	if err != nil {
+		panic(err)
+	}
+
+	hostname := parsedURL.Hostname()
+	port := parsedURL.Port()
+	path := parsedURL.RawPath
+	scheme := parsedURL.Scheme
+	return hostname, port, path, scheme
+}
+
+func (request Request) GetHostname() string {
+	hostname, _, _, _ := urlParser(request.URL)
+	return hostname
+}
+
+func (request Request) GetPort() string {
+	_, port, _, scheme := urlParser(request.URL)
+	if port == "" {
+		port = PortMap[scheme]
+	}
+	return port
+}
+
+func (request Request) GetPath() string {
+	_, _, path, _ := urlParser(request.URL)
+	if path == "" {
+		path = "/"
+	}
+	return path
+}
+
+func (request Request) GetAddress() string {
+	return net.JoinHostPort(request.GetHostname(), request.GetPort())
+}
+
+func (request Request) Serialize() (*bytes.Buffer, error) {
+
+	var buf bytes.Buffer
+
+	// write headers
+	_, err := fmt.Fprintf(&buf, "%s %s %s", request.Method, request.GetPath(), request.Protocol)
+	if err != nil {
+		return nil, err
+	}
+	buf.Write(CLRF)
+
+	for key, value := range request.Headers {
+		fmt.Fprintf(&buf, "%s: %s", key, value)
+		buf.Write(CLRF)
+	}
+
+	// terminate headers
+	buf.Write(CLRF)
+
+	// write Body
+	if len(request.Body) != 0 {
+		buf.Write(request.Body)
+	}
+
+	return &buf, nil
+
+}
